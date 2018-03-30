ebbd56630009646e4a0bdd6d6d539fd443cfc05d
John Mayfield <john@nextmovesoftware.com>
2018-03-30 18:45:44 +0100

    Flag to change rule 1b behaviour

--- core/src/main/java/com/simolecule/centres/rules/Rule1b.java
+++ core/src/main/java/com/simolecule/centres/rules/Rule1b.java
@@ -33,24 +33,35 @@ import com.simolecule.centres.Node;
  */
 public class Rule1b<A, B> extends SequenceRule<A, B> {
 
-  public Rule1b(BaseMol<A, B> mol)
-  {
+  /**
+   * Flag indicates whether to match the problematic
+   * IUPAC 2013 recommendations for Rule 1B.
+   */
+  private static final boolean IUPAC_2013 = false;
+
+  public Rule1b(BaseMol<A, B> mol) {
     super(mol);
   }
 
   @Override
-  public int compare(Edge<A, B> a, Edge<A, B> b)
-  {
-    if (a.getEnd().isSet(Node.RING_DUPLICATE) &&
-        b.getEnd().isSet(Node.RING_DUPLICATE))
+  public int compare(Edge<A, B> a, Edge<A, B> b) {
+    if (IUPAC_2013) {
       return -Integer.compare(a.getEnd().getDistance(),
                               b.getEnd().getDistance());
-    else {
-      if (a.getEnd().isSet(Node.RING_DUPLICATE) && !b.getEnd().isSet(Node.RING_DUPLICATE))
-        return +1;
-      if (!a.getEnd().isSet(Node.RING_DUPLICATE) && b.getEnd().isSet(Node.RING_DUPLICATE))
-        return -1;
-      return 0;
+    } else {
+      if (a.getEnd().isSet(Node.RING_DUPLICATE) &&
+          b.getEnd().isSet(Node.RING_DUPLICATE))
+        return -Integer.compare(a.getEnd().getDistance(),
+                                b.getEnd().getDistance());
+      else {
+        if (a.getEnd().isSet(Node.RING_DUPLICATE) && !b.getEnd()
+                                                       .isSet(Node.RING_DUPLICATE))
+          return +1;
+        if (!a.getEnd().isSet(Node.RING_DUPLICATE) && b.getEnd()
+                                                       .isSet(Node.RING_DUPLICATE))
+          return -1;
+        return 0;
+      }
     }
   }
 }
