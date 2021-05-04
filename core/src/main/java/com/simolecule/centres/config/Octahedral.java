52d347845aa5bf0fc88f0f1728de3e1782d528d9
John Mayfield <john@nextmovesoftware.com>
2021-05-04 15:51:08 +0100

    Improve OC-6 naming

--- core/src/main/java/com/simolecule/centres/config/Octahedral.java
+++ core/src/main/java/com/simolecule/centres/config/Octahedral.java
@@ -34,19 +34,44 @@ import com.simolecule.centres.Node;
 import com.simolecule.centres.rules.Priority;
 import com.simolecule.centres.rules.SequenceRule;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public final class Octahedral<A,B> extends Configuration<A,B> {
+public final class Octahedral<A, B> extends Configuration<A, B> {
 
-  private final Map<A,String> cache = new HashMap<>();
+  private final Map<A, String> cache = new HashMap<>();
 
   // the index of the carrier in the trans position
-  private final int[] TRANS_INDEX = new int[]{5,3,4,1,2,0};
+  private final int[] TRANS_INDEX = new int[]{5, 3, 4, 1, 2, 0};
 
-  public Octahedral(A focus, A[] carriers)
-  {
+  /* We are in normal form:
+   *        c
+   *        | a
+   *        |/
+   *    d---x---b = OH1
+   *       /|       where a: first carrier, b: second carried, etc
+   *      f |             x: focus
+   *        e             'a' is in front of the focus 'x', 'f' is behind
+   *
+   * storage index: a@0, b@1, c@2, d@3, e@4, f@5
+   *
+   * so if our axis is:
+   *   a-f (0->5) the plane atoms are b,c,d,e (1,2,3,4) anticlockwise
+   *   b-d (1->3) the plane atoms are a,c,f,e (0,2,5,4) anticlockwise
+   *   c-e (2->4) the plane atoms are a,d,f,b (0,3,5,1) anticlockwise
+   */
+  private final int[][] PLANE_INDEX = new int[][]{
+      {1, 2, 3, 4}, // a-f
+      {0, 2, 5, 4}, // b-d
+      {0, 3, 5, 1}, // c-e
+      {4, 5, 2, 0}, // d-b
+      {1, 5, 3, 0}, // e-c
+      {4, 3, 2, 1}, // f-a
+  };
+
+  public Octahedral(A focus, A[] carriers) {
     super(focus, carriers, 1);
   }
 
@@ -66,10 +91,25 @@ public final class Octahedral<A,B> extends Configuration<A,B> {
     throw new IllegalArgumentException();
   }
 
-  private int getPriorityNumber(List<List<Edge<A,B>>> parts, A atom) {
+  private A[] getPlane(A atom) {
+    A[] carriers = getCarriers();
+    for (int i = 0; i < carriers.length; i++) {
+      if (carriers[i].equals(atom)) {
+        return (A[]) new Object[]{
+            carriers[PLANE_INDEX[i][0]],
+            carriers[PLANE_INDEX[i][1]],
+            carriers[PLANE_INDEX[i][2]],
+            carriers[PLANE_INDEX[i][3]]
+        };
+      }
+    }
+    throw new IllegalArgumentException();
+  }
+
+  private int getPriorityNumber(List<List<Edge<A, B>>> parts, A atom) {
     int num = 1;
-    for (List<Edge<A,B>> part : parts) {
-      for (Edge<A,B> e : part) {
+    for (List<Edge<A, B>> part : parts) {
+      for (Edge<A, B> e : part) {
         if (e.getEnd().getAtom().equals(atom))
           return num;
       }
@@ -78,56 +118,65 @@ public final class Octahedral<A,B> extends Configuration<A,B> {
     throw new IllegalArgumentException();
   }
 
-  private boolean hasConfiguration(List<List<Edge<A,B>>> parts) {
+  private boolean hasConfiguration(List<List<Edge<A, B>>> parts) {
     return parts.size() > 2 ||
            parts.size() == 2 &&
            parts.get(0).size() != 1 && parts.get(0).size() != 5;
   }
 
   private Descriptor label(Node<A, B> root, SequenceRule<A, B> comp) {
-    List<Edge<A,B>>       edges    = root.getEdges();
-    Priority              priority = comp.sort(root, edges);
-    List<List<Edge<A,B>>> parts    = comp.getSorter().getGroups(edges);
+    List<Edge<A, B>>       edges    = root.getEdges();
+    Priority               priority = comp.sort(root, edges);
+    List<List<Edge<A, B>>> parts    = comp.getSorter().getGroups(edges);
 
     if (!hasConfiguration(parts))
       return Descriptor.ns; // maybe return unknown?
 
-    A fstAxisBeg = null, fstAxisEnd = null;
-    int n1  = 7;
-    int n2  = 7;
-    int n3  = 7;
+    A   fstAxisBeg = null;
+    int n1         = 0;
+    int n2         = 0;
 
-    for (Edge<A,B> edge : parts.get(0)) {
-      A beg = edge.getEnd().getAtom();
-      A end = findTransAtom(beg);
+    for (Edge<A, B> edge : parts.get(0)) {
+      A   beg = edge.getEnd().getAtom();
+      A   end = findTransAtom(beg);
       int num = getPriorityNumber(parts, end);
-      if (num < n1) {
-        n1 = num;
+      if (num > n1) {
+        n1         = num;
         fstAxisBeg = beg;
-        fstAxisEnd = end;
       }
     }
 
-    for (List<Edge<A,B>> part : parts) {
-      for (Edge<A, B> edge : part) {
-        A   beg = edge.getEnd().getAtom();
-        A   end = findTransAtom(beg);
-        if (beg.equals(fstAxisBeg) || beg.equals(fstAxisEnd) ||
-            end.equals(fstAxisBeg) || end.equals(fstAxisEnd))
-          continue;
-        int numBeg = getPriorityNumber(parts, beg);
-        int numEnd = getPriorityNumber(parts, end);
-        if (numBeg < n2) {
-          n2 = numBeg;
-          n3 = numEnd;
-        } else if (numBeg == n2 && numEnd < n3) {
-          n3 = numEnd;
-        }
+    List<Integer> ccw_plane = new ArrayList<>(4);
+    for (A a : getPlane(fstAxisBeg))
+      ccw_plane.add(getPriorityNumber(parts, a));
+
+    int low = 0;
+    for (int i = 1; i < 4; i++) {
+      if (ccw_plane.get(i) < ccw_plane.get(low)) {
+        low = i;
+      } else if (ccw_plane.get(i).equals(ccw_plane.get(low)) &&
+                 ccw_plane.get((i + 2) % 4) > ccw_plane.get((low + 2) % 4)) {
+        low = i;
       }
     }
+    n2 = ccw_plane.get((low + 2) % 4);
 
-    if (n1 < 7 && n3 < 7) {
-      cache.put(getFocus(), "OC-6-" + n1 + "" + n3);
+    int cmp = Integer.compare(ccw_plane.get((low + 1) % 4), ccw_plane.get((low + 4 - 1) % 4));
+    if (cmp == 0)
+      cmp = Integer.compare(ccw_plane.get((low + 1) % 4), ccw_plane.get((low + 4 - 1) % 4));
+    if (cmp == 0)
+      cmp = Integer.compare(ccw_plane.get((low + 2) % 4), ccw_plane.get((low + 4 - 2) % 4));
+
+    final String rotate;
+    if (cmp < 0)
+      rotate = "-A";
+    else if (cmp > 0)
+      rotate = "-C";
+    else
+      rotate = "";
+
+    if (n1 < 7 && n2 < 7) {
+      cache.put(getFocus(), "OC-6-" + n1 + "" + n2 + rotate);
       return Descriptor.OC_6;
     }
 
